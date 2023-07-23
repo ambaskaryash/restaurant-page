08e9c45367dbfe7315c9dce7eefbc0183975ae75
ambaskaryash <kimetsu119@gmail.com>
2023-07-23 17:22:25 +0530

    s

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -11,7 +11,8 @@ const FindUs = () => (
       <SubHeading title="Contact" />
       <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
       <div className="app__wrapper-content">
-        <p className="p__opensans">Shop no. 6, Magh Appts, Square, opposite Padole Hospital, Swavalambi Nagar, Nagpur, Maharashtra 440022</p>
+        <p className="p__opensans"><b>Address:</b> Shop no. 6, Magh Appts, Square, opposite Padole Hospital, Swavalambi Nagar, Nagpur, Maharashtra 440022</p><br />
+        <p className="p__opensans"><b>Contact Number</b> : +91 91456 46914</p>
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
         <p className="p__opensans">Mon - Sun: 12:00 pm - 10:30 pm</p>
         <p className="p__opensans">On Holidays: Timings may differ</p>
